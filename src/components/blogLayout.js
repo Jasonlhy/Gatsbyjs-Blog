480e61627d57dea4995d96d49d3a6583ce4fafb9
Jason <jasonlhy@gmail.com>
2019-06-09 02:13:23 +0800

    Use js hook to control the sidemenu

--- src/components/blogLayout.js
+++ src/components/blogLayout.js
@@ -11,46 +11,68 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer.js"
+import useMedia from "../hooks/use-media"
 import "./layout.css"
 
-const Layout = ({ header, left, right }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQueryInBlog {
-        site {
-          siteMetadata {
-            title
+const Layout = ({ header, left, right }) => {
+  const displayType = useMedia(
+    // Media queries
+    ["(min-width: 550px)"],
+    // Column counts (relates to above media queries by array index)
+    ["Desktop"],
+    // Default column count
+    "Mobile"
+  )
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQueryInBlog {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} isFullPageLayout={true} />
-        <div className="layoutSmallScreenPadding">
-          <div className="layout-header">{header}</div>
+      `}
+      render={data => (
+        <>
+          <Header
+            siteTitle={data.site.siteMetadata.title}
+            isFullPageLayout={true}
+          />
+          <div className="layoutSmallScreenPadding">
+            <div className="layout-header">{header}</div>
 
-          <div style={{ display: "flex", flexWrap: "wrap", marginBottom: "1rem" }}>
-            <main style={{ minWidth: "250px", flex: "1 0 250px" }}>{left}</main>
-            <div
-              style={{
-                flex: "0 0 250px",
-                marginLeft: "1.0875rem",
-                boxShadow: "rgba(0, 0, 0, 0.15) 1px 0px 5px",
-                marginRight: "-1.0875rem",
-                backgroundColor: "#f6f8fa"
-              }}
-            >
-              {right}
+            <div style={{ display: "flex", marginBottom: "1rem" }}>
+              <main style={{ minWidth: "250px", flex: "1 0 250px" }}>
+                {left}
+              </main>
+
+              {displayType === "Desktop" ? (
+                <div
+                  style={{
+                    flex: "0 0 250px",
+                    marginLeft: "1.0875rem",
+                    boxShadow: "rgba(0, 0, 0, 0.15) 1px 0px 5px",
+                    marginRight: "-1.0875rem",
+                    backgroundColor: "#f6f8fa",
+                  }}
+                >
+                  {right}
+                </div>
+              ) : (
+                right
+              )}
             </div>
-          </div>
 
-          <Footer />
-        </div>
-      </>
-    )}
-  />
-)
+            <Footer />
+          </div>
+        </>
+      )}
+    />
+  )
+}
 
 Layout.propTypes = {
   header: PropTypes.node.isRequired,
